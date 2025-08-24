21c8d1c6757c5abf22d0ef6657ed50322fc4382e
Ashutosh Singh <159605789+Ashutosh3678@users.noreply.github.com>
2025-08-24 20:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (43.64%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        int n = r.size();
+        vector<int> a(n, 1);
+        
+    
+        for(int i = 1; i < n; i++){
+            if(r[i] > r[i-1]){
+                a[i] = a[i-1] + 1;
+            }
+        }
+
+    
+        for (int i = n - 2; i >= 0; i--) {
+            if (r[i] > r[i+1]) {
+                a[i] = max(a[i], a[i+1] + 1);
+            }
+        }
+        
+    
+        int sum = 0;
+        for (int num : a) {
+            sum += num; 
+        }
+        return sum;
+    }
+};
