b2fae932823a4738cd205b6be01ef85b27a26d3d
Ashutosh Singh <159605789+Ashutosh3678@users.noreply.github.com>
2026-02-08 11:02:39 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int r = matrix.size();
-        int c = matrix[0].size();
+    void setZeroes(vector<vector<int>>& mat) {
+        int r = mat.size();
+        int c = mat[0].size();
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (matrix[i][j] == 0) {
+                if (mat[i][j] == 0) {
                     for (int k = 0; k < r; k++) {
-                        if (matrix[k][j] != 0) {
-                            matrix[k][j] = 'a';
+                        if (mat[k][j] != 0) {
+                            mat[k][j] = 'a';
                         }
                     }
 
                     for (int k = 0; k < c; k++) {
-                        if (matrix[i][k] != 0) {
-                            matrix[i][k] = 'a';
+                        if (mat[i][k] != 0) {
+                            mat[i][k] = 'a';
                         }
                     }
                 }
@@ -22,8 +22,8 @@ public:
         }
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (matrix[i][j] == 'a') {
-                    matrix[i][j] = 0;
+                if (mat[i][j] == 'a') {
+                    mat[i][j] = 0;
                 }
             }
         }
